ae795da352cb2bb2b2ea0540bdc67479121c8876
Yehor Smoliakov <yehors@ukr.net>
2022-12-07 21:51:16 +0200

    Update example.py

--- example.py
+++ example.py
@@ -3,7 +3,6 @@ import torch
 from scipy.io.wavfile import write as wavwrite
 
 from cleanunet.network import CleanUNet
-from cleanunet.denoiser import denoise
 
 # load the checkpoint
 checkpoint = torch.load('./DNS-large-full.pkl', map_location='cpu')
@@ -19,5 +18,8 @@ noisy_audio, sr = torchaudio.load('./demo/tsn_2.wav')
 # clean the noisy speech
 generated_audio = sampling(net, noisy_audio)
 
-# save cleaned speech
-wavwrite('tsn_2_enhanced.wav', 16_000, generated_audio[0].squeeze().cpu().numpy())
+# get the cleaned speech
+cleaned_audio = generated_audio[0].squeeze().cpu().numpy()
+
+# save the cleaned speech
+wavwrite('tsn_2_enhanced.wav', 16_000, cleaned_audio)
