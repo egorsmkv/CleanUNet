6a8ef54bd067e0b51949c319b562c54c9cf5341e
Yehor Smoliakov <yehors@ukr.net>
2022-12-07 21:53:35 +0200

    Update example.py

--- example.py
+++ example.py
@@ -7,6 +7,20 @@ from cleanunet.network import CleanUNet
 # load the checkpoint
 checkpoint = torch.load('./DNS-large-full.pkl', map_location='cpu')
 
+network_config = {
+        "channels_input": 1,
+        "channels_output": 1,
+        "channels_H": 64,
+        "max_H": 768,
+        "encoder_n_layers": 8,
+        "kernel_size": 4,
+        "stride": 2,
+        "tsfm_n_layers": 5, 
+        "tsfm_n_head": 8,
+        "tsfm_d_model": 512, 
+        "tsfm_d_inner": 2048
+}
+
 # build the network
 net = CleanUNet(**network_config)
 net.load_state_dict(checkpoint['model_state_dict'])
