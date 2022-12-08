2bf7509675aaf1d04e6b0a070827d4b4d1ba753e
Yehor Smoliakov <yehors@ukr.net>
2022-12-08 12:14:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="cleanunet",
-    version="0.0.1",
+    version="0.0.2",
     author="Yehor Smoliakov",
     author_email="egorsmkv@gmail.com",
     description="cleanunet",
