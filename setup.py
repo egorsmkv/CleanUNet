d1e9cf8b94df00699bf7552200ec63a0e9c69cc7
Yehor Smoliakov <yehors@ukr.net>
2022-12-08 11:32:44 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="cleanunet",
+    version="0.0.1",
+    author="Yehor Smoliakov",
+    author_email="egorsmkv@gmail.com",
+    description="cleanunet",
+    long_description="cleanunet",
+    long_description_content_type="text/markdown",
+    url="https://github.com/egorsmkv/CleanUNet",
+    packages=setuptools.find_namespace_packages(include=['cleanunet.*']),
+    python_requires=">=3.7",
+    install_requires=[
+        "torch",
+        "torchaudio",
+        "numpy",
+        "scipy",
+    ]
+)
